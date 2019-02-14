ee476cae322d0183ae72c9fc520647e4474a3bbb
Kennan Smith <kennan.smith1@gmail.com>
2019-02-13 19:44:10 -0700

    added delete functionality and basic related styling
    You can now delete list items by clicking on them and this functionality has corresponding styling (red background when hovering over list item to imply deletion upon click, a .2s ease-out transition to make the appearance smooth when an item is removed

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -4,10 +4,19 @@ class TodoItems extends Component {
   constructor(props) {
     super(props);
     this.createTasks = this.createTasks.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   createTasks(item) {
-    return <li key={item.key}>{item.text}</li>;
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
+  }
+
+  delete(key) {
+    this.props.delete(key);
   }
 
   render() {
--- src/TodoList.js
+++ src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
 
   addItem(e) {
@@ -33,6 +34,15 @@ class TodoList extends Component {
     console.log(itemArray);
   }
 
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function(item) {
+      return item.key !== key;
+    });
+    this.setState({
+      items: filteredItems
+    });
+  }
+
   render() {
     return (
       <div className="todoListMain">
@@ -45,7 +55,7 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items} />
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
       </div>
     );
   }
