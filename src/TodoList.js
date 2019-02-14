c5c072a4645668024b30f84a806da56338bea13f
Kennan Smith <kennan.smith1@gmail.com>
2019-02-13 18:44:15 -0700

    added code to make TodoList component affect the state
    set up state with 'items' array; defined 'addItem' func and bound it to state; made the form trigger addItem func upon submission; testing shows error messages in console, but the state is being correctly effected so I will ignore the errors for now

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,46 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+    e.preventDefault();
+
+    var itemArray = this.state.items;
+
+    if (this._inputElement !== "") {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this.inputElement.value = "";
+    }
+
+    console.log(itemArray);
+  }
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task" />
+          <form onSubmit={this.addItem}>
+            <input
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
+            />
             <button type="submit">add</button>
           </form>
         </div>
